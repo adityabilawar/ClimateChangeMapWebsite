290231aaca1f9c6bbcc2a6a0d7c575808b0d826a
Blue Putty <blueputty01@gmail.com>
2021-06-03 18:04:51 -0400

    did stuff

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,28 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFilenameHelpers } = require('webpack');
+
 
 module.exports = {
     entry: './src/js/index.js',
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    // [style-loader](/loaders/style-loader)
+                    { loader: 'style-loader' },
+                    // [css-loader](/loaders/css-loader)
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
