14722a2a5d3f25719e3769d246e2168127a7ab16
Blue Putty <blueputty01@gmail.com>
2021-06-03 17:26:43 -0400

    fixed webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/js/map.js',
+    entry: './src/js/index.js',
     plugins: [
         new HtmlWebpackPlugin({
             template: 'src/index.html'
