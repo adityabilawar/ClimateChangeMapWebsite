c255bd75c94e9df5942ce56859d1d1bd73bdd80a
Aditya Bilawar <23ab1107@wwprsd.org>
2021-06-28 17:39:46 -0400

    deleted useless import

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import '../css/main.css';
 import '../css/map.css';
 import Map from './Map';
 import form from './form';
-import contactFormData from 'c:/Users/Aditya/Documents/ClimateChangeMapWebsite/src/js/contactFormData';
+
 
 const map = new Map({ lat: 0, lng: 0 }, 2.7);
 
